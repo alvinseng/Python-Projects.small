a1313c0df0f66b777547afad3b39cf1ad1d74091
alvinseng <avinseng@gmail.com>
2023-12-03 21:44:30 -0800

    first

---
+++ main.py
@@ -0,0 +1,39 @@
+from turtle import Turtle, Screen
+import random
+
+t = Turtle()
+s = Screen()
+
+
+"""Turtle style and config"""
+t.shape("turtle")
+
+# for i in range(0, 100):
+#     tim.color("blue", "red", "green", "yellow")
+#     tim.speed(100)
+#     tim.begin_fill()
+#     tim.triangle(100)
+#     tim.end_fill()
+#     tim.penup()
+#     tim.forward(50)
+#     tim.pendown()
+
+
+# for i in range(100):
+#     tim.color("blue")
+#     tim.forward(10)
+#     tim.up()
+#     tim.forward(10)
+#     tim.pd()
+
+
+
+angle = 0
+color = ["blue", "red", "orange", "green", "yellow", "purple", "black"]
+t.pencolor()
+t.circle(100)
+
+
+
+"""Screen style and config"""
+s.exitonclick()
