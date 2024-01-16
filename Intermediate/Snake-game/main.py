482fc323fa6ba4c9c9ef48fde97a5fabeb137aa1
Alvin Seng <134309598+alvinseng@users.noreply.github.com>
2024-01-15 23:30:34 -0800

    Update main.py

--- Intermediate/Snake-game/main.py
+++ Intermediate/Snake-game/main.py
@@ -1,13 +1,26 @@
 from turtle import Turtle, Screen
+import time
 
 screen = Screen()
 screen.setup(width = 600, height = 600)
 screen.bgcolor("black")
-
+screen.title("Snake Game")
+screen.tracer(0)
 
 snake_pos = [(0,0), (-20,0), (-40,0)]
+
+segments = []
 
 for snake_body in snake_pos:
   new_snake = Turtle(shape = "square")
   new_snake.color("white")
+  new_snake.pu()
   new_snake.goto(snake_body)
+  segments.append(new_snake)
+
+game_is_on = True
+
+while game_is_on:
+    screen.update()
+    time.sleep(1)
+    for seg_num in range(start = ,stop = , step = ):
