b424e6888156a7d38a1f7daec1bb723f8663babd
alvinseng <avinseng@gmail.com>
2024-01-25 00:10:51 -0800

    updated the car_manager, player, scoreboard

--- turtle-crossing/car_manager.py
+++ turtle-crossing/car_manager.py
@@ -11,23 +11,23 @@ class CarManager(Turtle):
 
     def __init__(self):
         super().__init__()
-        self.car_spawn()
+        self.create_car()
 
-    def car_design(self):
-        self.shape("square")
-        self.pu()
-        self.turtlesize(stretch_wid=1, stretch_len=2)
+    def create_car(self):
         car_color = random.choices(COLORS)
+        self.pu()
+        self.goto(300,0)
+        self.shape("square")
         self.color(car_color)
-        self.seth(180)
+        self.turtlesize(stretch_wid=1, stretch_len=2)
 
-    def car_spawn(self):
-        random_y = random.randint(-240, 240)
-        for cars in range(random_y):
-            self.car_design()
-            self.goto(300, random_y)
+    def random_start(self):
+        self.goto(0, 300)
+        # for _ in range(0, 300):
+        #     self.create_car()
 
-    def car_move(self):
-        new_x = self.xcor() - MOVE_INCREMENT
-        new_y = self.ycor()
-        self.goto(new_x, new_y)
+    def move(self):
+        new_x = self.xcor() - STARTING_MOVE_DISTANCE
+        random_y = random.randint(-240,240)
+        self.goto(new_x, random_y)
+
--- turtle-crossing/main.py
+++ turtle-crossing/main.py
@@ -16,23 +16,28 @@ car = CarManager()
 screen.listen()
 screen.onkey(player.go_up, "Up")
 
+car_spawn = car.create_car()
+
 
 game_is_on = True
 while game_is_on:
     time.sleep(0.1)
     screen.update()
-
+    car.move()
 
     # player movement
     if player.ycor() > 280:
         score.score_keeping()
         player.spawn()
 
-    # Cars movement from right to left
-    if car.xcor() != -320:
-        car.car_move()
+    # for _ in range(6):
+    #     car_spawn
 
-    if player
+    # Cars movement from right to left
+    if car.xcor != -300:
+        car.create_car()
+
+    # if player
 
 
 
