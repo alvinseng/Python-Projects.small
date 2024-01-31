c63800afe3d51a795b31dc042dc4ad4868d5e26e
alvinseng <avinseng@gmail.com>
2024-01-31 13:58:41 -0800

    started code for guessing game

--- Day-25-exercise/US-state-game/main.py
+++ Day-25-exercise/US-state-game/main.py
@@ -3,11 +3,19 @@ import pandas
 
 screen = turtle.Screen()
 screen.title("U.S. States Game")
+image = "blank_states_img.gif"
+screen.addshape(image)
+turtle.shape(image)
 
 
+# def get_mouse_click_coor(x, y):
+#     print(x, y)
+#
+#
+# turtle.onscreenclick(get_mouse_click_coor)
 
 
+answer_state = screen.textinput(title="Guess the State", prompt="What's another state's name?")
+print(answer_state)
 
-
-
-screen.exitonclick()
+turtle.mainloop()
