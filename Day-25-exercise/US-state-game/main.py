404dc4ba81edf39182df82d58e4878f69cd50e13
alvinseng <avinseng@gmail.com>
2024-01-31 13:51:04 -0800

    completed exercise and added new assignment

---
+++ Day-25-exercise/US-state-game/main.py
@@ -0,0 +1,13 @@
+import turtle
+import pandas
+
+screen = turtle.Screen()
+screen.title("U.S. States Game")
+
+
+
+
+
+
+
+screen.exitonclick()
--- Day-25-exercise/main.py
+++ Day-25-exercise/main.py
@@ -16,6 +16,7 @@
 
 
 import pandas
+import pandas as pd
 
 # data = pandas.read_csv("weather_data.csv")
 # # print(data)
@@ -34,6 +35,14 @@ import pandas
 
 
 data = pandas.read_csv("2018_Central_Park_Squirrel_Census_-_Squirrel_Data.csv")
-fur_color = data["Primary Fur Color"]
-fur_mean = fur_color.sum()
-print(fur_mean)
+grey_squirrels_count = len(data[data["Primary Fur Color"] == "Gray"])
+red_squirrels_count = len(data[data["Primary Fur Color"] == "Cinnamon"])
+black_squirrels_count = len(data[data["Primary Fur Color"] == "Black"])
+
+data_dict = {
+    "Fur Color": ["Gray", "Cinnamon", "Black"],
+    "Count": [grey_squirrels_count, red_squirrels_count, black_squirrels_count]
+}
+
+df = pandas.DataFrame(data_dict)
+df.to_csv("squirrel_count.csv")
