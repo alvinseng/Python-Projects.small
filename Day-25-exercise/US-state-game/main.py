fe7266bea1bec05901038f4eb86d36ef318e862d
alvinseng <avinseng@gmail.com>
2024-02-08 14:03:34 -0800

    completed state game

--- Day-25-exercise/US-state-game/main.py
+++ Day-25-exercise/US-state-game/main.py
@@ -1,5 +1,8 @@
 import turtle
 import pandas
+from state_name import StateName
+
+FONT = ('Courier', 10, 'normal')
 
 screen = turtle.Screen()
 screen.title("U.S. States Game")
@@ -7,6 +10,7 @@ image = "blank_states_img.gif"
 screen.addshape(image)
 turtle.shape(image)
 
+state_name = turtle
 
 # def get_mouse_click_coor(x, y):
 #     print(x, y)
@@ -14,8 +18,38 @@ turtle.shape(image)
 #
 # turtle.onscreenclick(get_mouse_click_coor)
 
+# print(answer_state)
 
-answer_state = screen.textinput(title="Guess the State", prompt="What's another state's name?")
-print(answer_state)
+data = pandas.read_csv("50_states.csv")
+state_data = data["state"].to_list()
+
+
+states_list = []
+
+while len(states_list) < 50:
+    answer_state = screen.textinput(title=f"{len(states_list)}/50 States Correct",
+                                    prompt="What's another state's name?")
+    answer_state = answer_state.title()
+    state_coord = data[data.state == answer_state]
+
+    if answer_state == "Exit":
+        missed_states = []
+        for states in state_data:
+            if states not in states_list:
+                missed_states.append(states)
+        new_data = pandas.DataFrame(missed_states)
+        new_data.to_csv("States_to_learn.csv")
+        break
+    if answer_state in state_data:
+        states_list.append(answer_state)
+        name_turtle = turtle.Turtle()
+        name_turtle.hideturtle()
+        name_turtle.speed("fastest")
+        name_turtle.pu()
+        name_turtle.goto(int(state_coord.x), int(state_coord.y))
+        name_turtle.write(answer_state, align="center", font=FONT)
+    else:
+        continue
+
 
 turtle.mainloop()
--- Day-25-exercise/main.py
+++ Day-25-exercise/main.py
@@ -18,31 +18,33 @@
 import pandas
 import pandas as pd
 
-# data = pandas.read_csv("weather_data.csv")
-# # print(data)
-#
-# temp_list = data["temp"].to_list()
-# # temp_mean = data["temp"].mean()
+data = pandas.read_csv("weather_data.csv")
+# print(data)
+
+temp_list = data["temp"].to_list()
+# temp_mean = data["temp"].mean()
 # temp_max = data["temp"].max()
 # print(temp_max)
 
 # print(data[data.temp == temp_max])
 
-# monday = data[data.day == "Monday"]
-# monday_temp = monday.temp[0]
+monday = data[data.day == "Monday"]
+print(monday)
+monday_temp = monday.temp[0]
+print(monday_temp)
 # monday_temp_F = monday_temp * 9/5 + 32
 # print(monday_temp_F)
 
 
-data = pandas.read_csv("2018_Central_Park_Squirrel_Census_-_Squirrel_Data.csv")
-grey_squirrels_count = len(data[data["Primary Fur Color"] == "Gray"])
-red_squirrels_count = len(data[data["Primary Fur Color"] == "Cinnamon"])
-black_squirrels_count = len(data[data["Primary Fur Color"] == "Black"])
-
-data_dict = {
-    "Fur Color": ["Gray", "Cinnamon", "Black"],
-    "Count": [grey_squirrels_count, red_squirrels_count, black_squirrels_count]
-}
-
-df = pandas.DataFrame(data_dict)
-df.to_csv("squirrel_count.csv")
+# data = pandas.read_csv("2018_Central_Park_Squirrel_Census_-_Squirrel_Data.csv")
+# grey_squirrels_count = len(data[data["Primary Fur Color"] == "Gray"])
+# red_squirrels_count = len(data[data["Primary Fur Color"] == "Cinnamon"])
+# black_squirrels_count = len(data[data["Primary Fur Color"] == "Black"])
+#
+# data_dict = {
+#     "Fur Color": ["Gray", "Cinnamon", "Black"],
+#     "Count": [grey_squirrels_count, red_squirrels_count, black_squirrels_count]
+# }
+#
+# df = pandas.DataFrame(data_dict)
+# df.to_csv("squirrel_count.csv")
