dbb4a2b3cc205cc13ef56fb871962fead9ff3ddf
alvinseng <avinseng@gmail.com>
2024-03-26 13:34:51 -0700

    Font colors were not visible on GUI, included fill='black' to make font visible

--- flash-card-project-start/main.py
+++ flash-card-project-start/main.py
@@ -1,9 +1,6 @@
 BACKGROUND_COLOR = "#B1DDC6"
+FONT_COLOR = "#000000"
 FONT_NAME = "Ariel"
-CARD_FRONT = "images/card_front.png"
-CARD_BACK = "../images/card_back.png"
-WRONG_BUTTON = "../images/wrong.png"
-CORRECT_BUTTON = "../images/right.png"
 
 # ----- Words function ------
 
@@ -11,26 +8,29 @@ CORRECT_BUTTON = "../images/right.png"
 
 # ------ UI Setup -------
 from tkinter import *
-import random
 
 window = Tk()
 window.title("Flashy")
 window.config(pady=50, padx=50, bg=BACKGROUND_COLOR)
 
-canvas = Canvas(width=800, height=526, highlightthickness=0)
+canvas = Canvas(width=800, height=526)
 flashcard_img = PhotoImage(file="images/card_front.png")
-canvas.create_image(400, 263, image=flashcard_img, bg=BACKGROUND_COLOR)
-flashcard_top_txt = canvas.create_text(400, 150, text="French", font=(FONT_NAME, 40, "italic"))
+canvas.create_image(400, 263, image=flashcard_img)
+canvas.create_text(400, 150, text="French", fill='black', font=(FONT_NAME, 40, "italic"))
+canvas.create_text(400, 263, text='Words here', fill='black', font=(FONT_NAME, 60, "bold"))
+canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
 canvas.grid(column=0, row=0, columnspan=2)
 
-flashcard_french_words = canvas.create_text(400, 263, text='Enter french words here', font=(FONT_NAME, 60, "bold"))
 
 wrong_img = PhotoImage(file="images/wrong.png")
-wrong_button = Button(image=wrong_img, highlightthickness=0)
+wrong_button = Button(image=wrong_img)
+wrong_button.config(highlightbackground=BACKGROUND_COLOR, highlightthickness=0)
 wrong_button.grid(column=0, row=1)
 
+
 correct_img = PhotoImage(file="images/right.png")
-correct_button = Button(image=correct_img, highlightthickness=0)
+correct_button = Button(image=correct_img)
+correct_button.config(highlightthickness=0, highlightbackground=BACKGROUND_COLOR)
 correct_button.grid(column=1, row=1)
 
 
