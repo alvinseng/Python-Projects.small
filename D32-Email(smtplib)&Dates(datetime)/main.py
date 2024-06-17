adc9a6fbfee0a6d79a3c5661827646c417fba456
alvinseng <avinseng@gmail.com>
2024-06-17 10:54:55 -0700

    SMTPLIB project, sending motivational quote

--- D32-Email(smtplib)&Dates(datetime)/main.py
+++ D32-Email(smtplib)&Dates(datetime)/main.py
@@ -1,25 +1,24 @@
-# import smtplib
-#
-# my_email = "kuromew84@gmail.com"
-# password = "hvhnqkmtulbpptme"
-#
-# with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
-#     connection.starttls()
-#     connection.login(user=my_email,password=password)
-#     connection.sendmail(
-#         from_addr=my_email,
-#         to_addrs="kuromew@myyahoo.com",
-#         msg="Subject:Hello\n\nThis is the body of my email"
-#     )
-
+import smtplib
 import datetime as dt
+import random
+
+MY_EMAIL = "kuromew84@gmail.com"
+PASSWORD = "hvhnqkmtulbpptme"
 
 now = dt.datetime.now()
-year = now.year
-month = now.month
-day_of_the_week = now.weekday()
-# print(day_of_the_week)
+weekday = now.weekday()
+if weekday == 0:
+    with open("quotes.txt") as quote_file:
+        all_quotes = quote_file.readlines()
+        quote = random.choice(all_quotes)
+    print(quote)
+    with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
+        connection.starttls()
+        connection.login(user=my_email,password=password)
+        connection.sendmail(
+            from_addr=my_email,
+            to_addrs="kuromew@myyahoo.com",
+            msg=f"Subject:Weekly Motivation\n\n{quote}"
+        )
 
-date_of_birth = dt.datetime(year=1994, month= 6, day=15)
-print(date_of_birth)
 
