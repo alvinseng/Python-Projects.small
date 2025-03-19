2a1449b2204314174af730d9d0ff565bdd6352b3
Alvin Seng <134309598+alvinseng@users.noreply.github.com>
2025-03-18 18:12:20 -0700

    Update main.py

--- Stocks News Alert/main.py
+++ Stocks News Alert/main.py
@@ -11,10 +11,6 @@ STOCK_API_KEY = "4GTT7KT142M2M2J7"
 NEWS_API_KEY = "68536e6013514aacb7f4d76e8b9ddbd4"
 
 
-    ## STEP 1: Use https://www.alphavantage.co/documentation/#daily
-# When stock price increase/decreases by 5% between yesterday and the day before yesterday then print("Get News").
-
-#TODO 1. - Get yesterday's closing stock price. Hint: You can perform list comprehensions on Python dictionaries. e.g. [new_value for (key, value) in dictionary.items()]
 
 stock_params = {
     "function": "TIME_SERIES_DAILY",
@@ -29,40 +25,30 @@ yesterday_data = data_list[0]
 yesterday_closing_price = yesterday_data["4. close"]
 # print(yesterday_closing_price)
 
-#TODO 2. - Get the day before yesterday's closing stock price
 day_before_yesterday = data_list[1]
 dby_closing = day_before_yesterday["4. close"]
 # print(dby_closing)
 
-#TODO 3. - Find the positive difference between 1 and 2. e.g. 40 - 20 = -20, but the positive difference is 20. Hint: https://www.w3schools.com/python/ref_func_abs.asp
 difference = abs(float(yesterday_closing_price) - float(dby_closing))
 
-#TODO 4. - Work out the percentage difference in price between closing price yesterday and closing price the day before yesterday.
 diff_percent = difference / float(yesterday_closing_price) * 100
 print(diff_percent)
 
-#TODO 5. - If TODO4 percentage is greater than 5 then print("Get News").
 if diff_percent > 2:
     news_params = {
         "apikey" : NEWS_API_KEY,
         "qInTitle": COMPANY_NAME,
     }
 
-#TODO 6. - Instead of printing ("Get News"), use the News API to get articles related to the COMPANY_NAME.
     news_response = requests.get(NEWS_ENDPOINT, params=news_params)
     articles = news_response.json()["articles"]
-#TODO 7. - Use Python slice operator to create a list that contains the first 3 articles. Hint: https://stackoverflow.com/questions/509211/understanding-slice-notation
     three_articles = articles[:3]
 
-# - Create a new list of the first 3 article's headline and description using list comprehension.
     formatted_articles = [f"Headline: {article['title']}. \nBrief: {article['description']}" for article in three_articles]
     print(formatted_articles)
 
-#TODO 9. - Send each article as a separate message via Twilio. 
 
 
-
-#Optional TODO: Format the message like this: 
 """
 TSLA: 🔺2%
 Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
